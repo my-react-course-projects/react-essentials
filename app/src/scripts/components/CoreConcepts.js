2bcf3ef87da8630604cae16ce2bee13048012bec
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-06 19:41:04 +0530

    making alt tag dynamic

--- app/src/scripts/components/CoreConcepts.js
+++ app/src/scripts/components/CoreConcepts.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CoreConcepts = ({title, description, image}) => {
     return (
         <li>
-            <img src={image} alt='{title}' />
+            <img src={image} alt={title} />
             <h3>{title}</h3>
             <p>{description}</p>
         </li>
