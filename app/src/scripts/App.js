8976714347098405c788cc623bd573ffc930a96a
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-06 19:40:26 +0530

    change filename

--- app/src/scripts/App.js
+++ app/src/scripts/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import Header from './components/Header'
-import Main from './components/Main'
+import Main from './components/MainWrapper'
 import CoreConcept from './components/CoreConcept'
 import Examples from './components/Examples'
 
