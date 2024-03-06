cad0c202e25dedbdb1c546d9877f31bd86e8b69d
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-06 19:44:56 +0530

    format the tab button component

--- app/src/scripts/components/TabButton.js
+++ app/src/scripts/components/TabButton.js
@@ -1,7 +1,13 @@
 import React from "react"
 
-const TabButton = ({children, onSelect, isSelected }) => {
-    return <li><button className={isSelected ? 'active' : undefined } onClick={onSelect}>{children}</button></li>
+const TabButton = ({children, isSelected, ...props }) => {
+    return (
+        <li>
+            <button className={isSelected ? 'active' : undefined } {...props}>
+                {children}
+            </button>
+        </li>
+    )
 }
 
 export default TabButton
